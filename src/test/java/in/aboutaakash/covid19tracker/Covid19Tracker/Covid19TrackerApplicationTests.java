a075354b90c29e1afae78fc948ea74011d7acae8
aakashverma1124 <info.aakash11@gmail.com>
2020-04-15 20:19:25 +0530

    reinitialized files

---
+++ src/test/java/in/aboutaakash/covid19tracker/Covid19Tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aboutaakash.covid19tracker.Covid19Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
